489da2d49573e84fc289c4590f4fd219d5847d88
supreetkaur <supreet97@gmail.com>
2024-04-17 14:58:18 -0700

    api to get version and fileURL

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
-
+// const useSearchParams = require("useSearchParams");
 const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log("Get on server");
+});
+
+app.get("/settings", (req, res) => {
+  console.log("req", req);
+  let searchParams = req.query;
+  console.log("version", searchParams.version);
+  console.log("fileURL", searchParams.fileURL);
 });
 
 app.listen(port, (req, res) => {
