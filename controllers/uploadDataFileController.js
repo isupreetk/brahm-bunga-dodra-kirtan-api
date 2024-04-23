5d7a97c99fce4267973327da4693ecc894a915a9
supreetkaur <supreet97@gmail.com>
2024-04-23 16:06:50 -0700

    version update with every file upload

--- controllers/uploadDataFileController.js
+++ controllers/uploadDataFileController.js
@@ -3,10 +3,27 @@ const knex = require("knex")(require("../knexfile"));
 
 const createDataFile = (req, res) => {
   const file = req.file;
+  let value;
 
   console.log("file", file);
   console.log("filename", file.filename);
-  // res.send("file saved on server");
+
+  const getValueWrapper = async () => {
+    const getValue = async () => {
+      return knex("settings")
+        .where("key", "Version")
+        .then((data) => {
+          console.log("data", data);
+          return data[0].value;
+        })
+        .catch((error) => {
+          return error;
+        });
+    };
+    value = await getValue();
+    console.log("value", value);
+  };
+  getValueWrapper();
 
   return knex("settings")
     .upsert({
@@ -15,7 +32,17 @@ const createDataFile = (req, res) => {
       // `https://easyservices-cb714e81a4fb.herokuapp.com/data/${file.filename}`,
     })
     .then((data) => {
-      return res.json(data);
+      knex("settings")
+        .upsert({
+          key: "Version",
+          value: parseInt(value) + 1,
+        })
+        .then((data) => {
+          return res.json(data);
+        })
+        .catch((error) => {
+          return res.send(error);
+        });
     })
     .catch((error) => {
       return res.send(error);
