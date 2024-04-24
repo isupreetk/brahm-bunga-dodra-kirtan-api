9c2d1f1c3c495f4b5d53d3fda324d1d74b6f8c55
supreetkaur <supreet97@gmail.com>
2024-04-24 13:40:16 -0700

    changed destination for multer file upload

--- routes/uploadDataFileRoute.js
+++ routes/uploadDataFileRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const uploadDataFileController = require("../controllers/uploadDataFileController");
 const multer = require("multer");
-const upload = multer({ dest: "data/" });
+const upload = multer({ dest: "/app/data/" });
 const fs = require("fs");
 
 router
