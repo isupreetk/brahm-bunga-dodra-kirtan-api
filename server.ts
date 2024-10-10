b61d335006058c7712ccbe42403376181eac8b83
supreetkaur <supreet97@gmail.com>
2024-10-10 17:40:12 -0400

    deleted commented out code

--- server.ts
+++ server.ts
@@ -1,24 +1,3 @@
-// import express, {Express, Request, Response} from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-
-// const app: Express = express();
-
-// dotenv.config();
-// const port = process.env.PORT;
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.get("/", (req: Request, res: Response) => {
-//   console.log("Get on server");
-//   return res.send("Welcome to Kirtan Server");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Connected on ${port}`);
-// });
-
 import express from "express";
 import keyFetchingRoute from "./src/routes/fetchKeyRouter";
 import updateFileURLRoute from "./src/routes/updateFileURLRouter";
